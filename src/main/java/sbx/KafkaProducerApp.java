4b76434b19fda8f2ae0f3966036aff3bd97f35ed
vvs-tech <st45.vvs@gmail.com>
2025-02-14 17:21:12 +0300

    producer 1

--- src/main/java/sbx/KafkaProducerApp.java
+++ src/main/java/sbx/KafkaProducerApp.java
@@ -1,4 +1,13 @@
 package main.java.sbx;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
 public class KafkaProducerApp {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducerApp.class);
+
+    public static void main(String[] args) throws Exception {
+
+    }
 }
