8d023b16eef4389ca8e59dbb3e333e75c8cc42fd
vvs-tech <st45.vvs@gmail.com>
2025-02-15 13:35:41 +0300

    producer 2

--- src/main/java/sbx/KafkaProducerApp.java
+++ src/main/java/sbx/KafkaProducerApp.java
@@ -1,13 +1,48 @@
 package main.java.sbx;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
 
 public class KafkaProducerApp {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducerApp.class);
 
+
     public static void main(String[] args) throws Exception {
+
+        Properties properties = new Properties();
+
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092, localhost:39092, localhost:49092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+//        properties.put(ProducerConfig.ACKS_CONFIG, "0");
+
+        try(Producer<String, String> producer= new KafkaProducer<String, String>(properties)){
+
+//          Отправить сообщение в топик sandbox
+            producer.send(new ProducerRecord<>("sandbox", "Hello java 7"));
+
+
+//          Отправить сообщение в топик sandbox и получить метаданные сообщения
+            RecordMetadata metadata =  producer.send(new ProducerRecord<>("sandbox", "Hello java 9")).get();
+            LOGGER.info("=============== Получить метаданные сообщения ===========================");
+            LOGGER.info("Metadata {}", metadata);
+            LOGGER.info("================ Получить метаданные сообщения ==========================");
+//==========================================
+//Metadata sandbox-1@1    //в логе в полученной метадате указан топик sandbox, партиция 1, и  смещение offset 1
+//==========================================
+
+            RecordMetadata metadataKey =  producer.send(new ProducerRecord<>("sandbox", "key-1","Hello java key-1 Msg: 1")).get();
+            LOGGER.info("================== Получить метаданные сообщения с указанным ключом ========================");
+            LOGGER.info("Metadata {}", metadataKey);
+            LOGGER.info("================== Получить метаданные сообщения с указанным ключом  ========================");
+
+
+        }
 
     }
 }
---
+++ src/main/java/sbx/tmp/Tmp.java
@@ -0,0 +1,39 @@
+package main.java.sbx.tmp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class Tmp {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Tmp.class);
+
+
+    public static void main(String[] args) throws Exception {
+
+        Properties properties = new Properties();
+
+
+
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092, localhost:39092, localhost:49092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+//        properties.put(ProducerConfig.ACKS_CONFIG, "0");
+
+        try(Producer<String, String> producer= new KafkaProducer<String, String>(properties)){
+            producer.send(new ProducerRecord<>("sandbox", "Hello java 8"));
+            RecordMetadata metadata =  producer.send(new ProducerRecord<>("sandbox", "Hello java 9")).get();
+
+            LOGGER.info("==========================================");
+            LOGGER.info("Metadata {}", metadata);
+            LOGGER.info("==========================================");
+
+
+        }
+
+    }
+}
